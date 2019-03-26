55f665c51f24af5621f901e22ec78c8dd60e324d
Sébastien Mazurais <SMazurais@CONDATE.com>
2019-03-26 17:50:39 +0100

    ajout du tracer

--- index.js
+++ index.js
@@ -1,82 +1,158 @@
 import flyd from './src/flyd';
 import { P, S, PS } from './src/patchinko';
+import m from './src/mithril';
+import meiosisTracer from './src/meiosis-tracer.min.js'
+
 
 var conditions = {
     initialState: {
-        conditions: {
-            precipitations: false,
-            sky: "Sunny"
-        }
+      conditions: {
+        precipitations: false,
+        sky: "Sunny"
+      }
     },
-    actions: function (update) {
-        return {
-            togglePrecipitations: function (value) {
-                update({ conditions: PS({ precipitations: value }) });
-            },
-            changeSky: function (value) {
-                update({ conditions: PS({ sky: value }) });
-            }
-        };
+    actions: function(update) {
+      return {
+        togglePrecipitations: function(value) {
+          update({ conditions: PS({ precipitations: value }) });
+        },
+        changeSky: function(value) {
+          update({ conditions: PS({ sky: value }) });
+        }
+      };
     }
-};
-
-var convert = function (value, to) {
-    return Math.round(
-        (to === "C") ? ((value - 32) / 9 * 5) : (value * 9 / 5 + 32)
+  };
+  
+  var skyOption = function({ state, actions, value, label }) {
+    return m("label",
+      m("input", { type: "radio", id: value, name: "sky",
+        value, checked: state.conditions.sky === value,
+        onchange: evt => actions.changeSky(evt.target.value)
+      }),
+      label
     );
-};
-
-var temperature = {
-    initialState: {
-        temperature: {
-            value: 22,
-            units: "C"
-        }
+  };
+  
+  var Conditions = {
+    view: function(vnode) {
+      var { state, actions } = vnode.attrs;
+      return m("div",
+        m("label",
+          m("input", {
+            type: "checkbox",
+            checked: state.conditions.precipitations,
+            onchange: evt =>
+              actions.togglePrecipitations(evt.target.checked)
+          }),
+          "Precipitations"
+        ),
+        m("div",
+          skyOption({ state, actions, value: "SUNNY",
+            label: "Sunny"}),
+          skyOption({ state, actions, value: "CLOUDY",
+            label: "Cloudy"}),
+          skyOption({ state, actions, value: "MIX",
+            label: "Mix of sun/clouds"})
+        )
+      );
+    }
+  };
+  
+  var convert = function(value, to) {
+    return Math.round(
+      (to === "C") ? ((value - 32) / 9 * 5) : (value * 9 / 5 + 32)
+    );
+  };
+  
+  var temperature = {
+    initialState: function(label) {
+      return {
+        label,
+        value: 22,
+        units: "C"
+      };
     },
-    actions: function (update) {
-        return {
-            increment: function (amount) {
-                update({ temperature: PS({ value: S(x => x + amount) }) });
-            },
-            changeUnits: function () {
-                update({
-                    temperature: S(state => {
-                        var value = state.value;
-                        var newUnits = state.units === "C" ? "F" : "C";
-                        var newValue = convert(value, newUnits);
-                        state.value = newValue;
-                        state.units = newUnits;
-                        return state;
-                    })
-                });
-            }
-        };
+    actions: function(update) {
+      return {
+        increment: function(id, amount) {
+          update({ [id]: PS({ value: S(x => x + amount) }) });
+        },
+        changeUnits: function(id) {
+          update({
+            [id]: S(state => {
+              var value = state.value;
+              var newUnits = state.units === "C" ? "F" : "C";
+              var newValue = convert(value, newUnits);
+              state.value = newValue;
+              state.units = newUnits;
+              return state;
+            })
+          });
+        }
+      };
     }
-};
-
-var app = {
-    initialState: P({},
-        conditions.initialState,
-        temperature.initialState
+  };
+  
+  var Temperature = {
+    view: function(vnode) {
+      var { state, id, actions } = vnode.attrs;
+      return m("div",
+        state[id].label, " Temperature: ",
+        state[id].value, m.trust("&deg;"), state[id].units,
+        m("div",
+          m("button",
+            { onclick: () => actions.increment(id, 1) },
+            "Increment"),
+          m("button",
+            { onclick: () => actions.increment(id,-1) },
+            "Decrement")
+        ),
+        m("div",
+          m("button",
+            { onclick: () => actions.changeUnits(id) },
+            "Change Units")
+        )
+      );
+    }
+  };
+  
+  var app = {
+    initialState: Object.assign({},
+      conditions.initialState,
+      { air: temperature.initialState("Air") },
+      { water: temperature.initialState("Water") }
     ),
-    actions: function (update) {
-        return P({},
-            conditions.actions(update),
-            temperature.actions(update)
-        );
+    actions: function(update) {
+      return Object.assign({},
+        conditions.actions(update),
+        temperature.actions(update)
+      );
     }
-};
+  };
+  
+  var App = {
+    view: function(vnode) {
+      var { state, actions } = vnode.attrs;
+      return m("div",
+        m(Conditions, { state, actions }),
+        m(Temperature, { state, id: "air", actions }),
+        m(Temperature, { state, id: "water", actions }),
+        m("pre", JSON.stringify(state, null, 4))
+      );
+    }
+  };
+  
+  var update = flyd.stream();
+  var states = flyd.scan(P, app.initialState, update);
 
-var update = flyd.stream();
-var states = flyd.scan(P, app.initialState, update);
-
+  meiosisTracer({
+    selector: "#tracer",
+    streams: [ update ]
+  })
+  
 var actions = app.actions(update);
-// states.map(function (state) {
-//     document.write("<pre>" + JSON.stringify(state, null, 2) + "</pre>");
-// });
 
-states.map((state) => {
-    console.log(JSON.stringify(state));
-    var elKpi = document.getElementById('kpi');
-    elKpi.innerHTML = JSON.stringify(state, null, 2);
-});
+  m.mount(document.getElementById("app"), {
+    view: () => m(App, { state: states(), actions })
+  });
+  
